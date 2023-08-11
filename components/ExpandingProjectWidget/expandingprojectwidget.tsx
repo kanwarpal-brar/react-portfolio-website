52da31c7250cb6ea8aaa8584808bfc4ebabe18ba
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2023-08-10 22:54:17 -0400

    - Basic setup for expanding project widget

--- components/ExpandingProjectWidget/expandingprojectwidget.tsx
+++ components/ExpandingProjectWidget/expandingprojectwidget.tsx
@@ -1,7 +1,18 @@
 import styles from "./expandingprojectwidget.module.scss"
+import Image from "next/image"
 
-export default function ExpandingProjectWidget() {
+export type ExpandingProjectWidgetProps = {
+    title: string;
+    img: string;
+    desc: string;
+    link: string;
+}
+
+export default function ExpandingProjectWidget({ title, img, desc, link }: ExpandingProjectWidgetProps) {
   return (
-    <div>Ok</div>
+    <div className={styles.container}>
+        <h2>{title}</h2>
+        <Image src={img} alt={`Image for ${title} project`}/>
+    </div>
   )
 }
