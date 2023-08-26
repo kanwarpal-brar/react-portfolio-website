c879350458c7f388e5ab12e1dbc58167f94c1fab
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2023-08-25 21:01:43 -0400

    - Temporarily removed modal and readme parse from project widgets

--- components/ExpandingProjectWidget/expandingprojectwidget.tsx
+++ components/ExpandingProjectWidget/expandingprojectwidget.tsx
@@ -1,7 +1,7 @@
 import { Modal } from "@mui/material";
 import styles from "./expandingprojectwidget.module.scss"
 import Image from "next/image"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import projectConfig from "../../public/targetProjects.json";
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from "remark-gfm";
@@ -13,14 +13,16 @@ export type ExpandingProjectWidgetProps = {
     link: string;
     style?: {[key: string]: string};
     tags?: string[]
+    useModal?: boolean;
 }
 
-export default function ExpandingProjectWidget({ title, desc, link, style, tags }: ExpandingProjectWidgetProps) {
+export default function ExpandingProjectWidget({ title, desc, link, style, tags, useModal }: ExpandingProjectWidgetProps) {
 	const [open, setOpen] = useState(false)
   let debounce = false
   const displayTags = tags ? tags : []
   const [modelDesc, setModelDesc] = useState("")
   const imgUrl = `${projectConfig.repoImageUrl}/${title.toLowerCase()}.png`
+  const expanding = useModal ? useModal : false
 
   function extractDesc(rawText: string): string {
     const results = rawText.match(/<!--start-->(.*?)<!--end-->/s)
@@ -45,36 +47,40 @@ export default function ExpandingProjectWidget({ title, desc, link, style, tags 
     setTimeout(() => {debounce = false}, 50)
 	}
 
-  return (
-    <div className={styles.container} onClick={handleOpenByContainer}>
-        <h3>{title}</h3>
-        <div className={styles.desc_box}>
-          <p>{desc}</p>
-        </div>
-        <div className={styles.tag_block}>
-            {displayTags.map((tag, i) => {
-              return <div key={i} className={styles.tag}>{tag}</div>
-            })}
+  function body() {
+    return (
+      <div className={styles.container} onClick={expanding ? handleOpenByContainer : undefined}>
+          <h3>{title}</h3>
+          <div className={styles.desc_box}>
+            <p>{desc}</p>
           </div>
-
-        <Modal open={open} onClose={handleClose} className={styles.modal}>
-          <div className={styles.modal_container}>
-            <div className={styles.modal_content}>
-              <div className={styles.markdown_box}>
-                <div className={styles.img_box}>
-                  <Image src={modelDesc ? imgUrl : "/SMPTE_Color_Bars.svg"} alt={`Image for ${title} project`} width={250} height={250}/>
+          <div className={styles.tag_block}>
+              {displayTags.map((tag, i) => {
+                return <div key={i} className={styles.tag}>{tag}</div>
+              })}
+            </div>
+  
+          <Modal open={open} onClose={handleClose} className={styles.modal}>
+            <div className={styles.modal_container}>
+              <div className={styles.modal_content}>
+                <div className={styles.markdown_box}>
+                  <div className={styles.img_box}>
+                    <Image src={modelDesc ? imgUrl : "/SMPTE_Color_Bars.svg"} alt={`Image for ${title} project`} width={250} height={250}/>
+                  </div>
+                  <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                    {modelDesc ? modelDesc : `# ${title}\n${desc}`}
+                  </ReactMarkdown>
                 </div>
-                <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                  {modelDesc ? modelDesc : `# ${title}\n${desc}`}
-                </ReactMarkdown>
+              </div>
+              <div className={styles.button_box}>
+                <button onClick={handleClose}>Close</button>
+                <Link href={link} target="_blank"><button>Github</button></Link>
               </div>
             </div>
-            <div className={styles.button_box}>
-              <button onClick={handleClose}>Close</button>
-              <Link href={link} target="_blank"><button>Github</button></Link>
-            </div>
-          </div>
-        </Modal>
-    </div>
-  )
+          </Modal>
+      </div>
+    )
+  }
+
+  return (expanding ? body() : <Link href={link} target="_blank">{ body() }</Link>)
 }
