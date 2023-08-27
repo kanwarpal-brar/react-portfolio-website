4beb19a16308e7f001e4446817eac96b5c41369b
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2023-08-27 19:00:43 -0400

    - updated config

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'https://kanwarpal-github-repos.imgix.net',
         port: '',
-        pathname: '',
+        pathname: '/',
       },
     ],
   },
