1ecc13df5abf5e1ffc6864cc30785fd4c6011fb1
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2023-08-30 10:56:31 -0400

    - temporarily removed sidebar on mobile

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,10 +1,12 @@
 import SideBar from "../Sidebar/sidebar";
 import styles from "./layout.module.scss";
+import { useMediaQuery } from "@mui/material";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const isMobile = useMediaQuery("(orientation: portrait") ? true : false
   return (
     <div className={styles.layout_container}>
-      <SideBar />
+      { isMobile ? undefined : <SideBar />}
       <main className={styles.content_container}>{children}</main>
     </div>
   );
