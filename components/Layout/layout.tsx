7aafad9223a6eb0ec80c2f3a9d84a9235baf6246
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2023-09-19 02:17:30 -0400

    - added google analytics script

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import SideBar from "../Sidebar/sidebar";
 import styles from "./layout.module.scss";
 import { useMediaQuery } from "@mui/material";
@@ -6,6 +7,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const isMobile = useMediaQuery("(orientation: portrait", {noSsr: true}) ? true : false
   return (
     <div className={styles.layout_container}>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=YOUR ID"
+      ></Script>
+      <Script strategy="afterInteractive" id="gtm">
+        {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-6Y5ES8TQ3X',{ 'debug_mode':true });
+        `}
+      </Script>
       <SideBar isMobile={isMobile}/>
       <main className={styles.content_container}>{children}</main>
     </div>
