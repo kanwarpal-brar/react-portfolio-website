9d1aaa91e0309f3c401da07c4489c77166bc142a
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2024-03-15 21:33:33 -0400

    Updated resume file and experience secton. Updated packages

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,21 +11,29 @@ import ExtraCurricularBox, {
 import projectConfig from "../public/targetProjects.json";
 
 const aboutBlurb =
-"Hi, I'm Kanwarpal, Welcome to my portfolio website. I'm a lifetime learner and aspiring software developer in my 3rd year of an undergraduate Computer Science degree from the University of Waterloo. I have had the opportunity to intern at numerous software companies and interact with many industry-standard technologies, but I'm always looking for more to learn."
+"Hi, I'm Kanwarpal, Welcome to my portfolio website. I'm a lifetime learner and aspiring software developer in my 4th year of an undergraduate Computer Science degree from the University of Waterloo. I have had the opportunity to intern at numerous software companies and interact with many industry-standard technologies, but I'm always looking for more to learn."
 const resumeBlurb =
   "Take a look at my resume, and feel free to contact me with questions and opportunities of any nature.";
 
 const workExperience: ExpBoxProps[] = [
   {
+    title: "Backend Software Engineering Co-op",
+    company: "Carta",
+    date: "Jan 2024 - Present",
+    bullets: [
+      "Implemented lightweight financial report variant reducing report generation time by 12% and elevating user experience",
+      "Leading the migration and optimization of report generation code using Apache POI, gRPC + Protobuf for communication, and Java concurrency functionality to speed up user experience",
+    ],
+  },
+  {
     title: "Software Developer",
     company: "Arctic Wolf Networks",
     date: "May - Aug 2023",
     bullets: [
-      "Developed a concurrency-safe Prometheus metrics monitoring system in Go for a custom Apache Kafka wrapper",
-      "Played a pivotal role in the v1.0.0 release of a Golang Apache Kafka wrapper, decoupling systems organization-wide",
-      "Spearheaded design & development of Subscriber & Deserializer systems for in-house Python Kafka wrapper",
-      "Implemented custom Subscriber & Deserializer interface logic based on Apache Avro Schema versions",
-      "Developed REST APIs for numerous AWS microservices in Python and Go, adhering to OpenAPI specification",
+      "Reduced response times 25% by designing and developing a concurrent Prometheus metrics monitoring system in Go for an Apache Kafka Wrapper, contributing to a successful release",
+      "Designed a reflection-based Go Test verification algorithm, identifying missing/broken metrics tests with 100% accuracy",
+      "Identified, investigated, and planned fix for a REST API bug across multiple AWS microservices caused by improper adherence to OpenAPI specification",
+      "Reduced lead times by designing a forwards/backwards compatible Kafka Serialization system using SchemaVer and Avro",
     ],
   },
   {
@@ -33,21 +41,20 @@ const workExperience: ExpBoxProps[] = [
     company: "Genesys Laboratories",
     date: "Sept - Dec 2022",
     bullets: [
-      "Demonstrated exceptional skill with AWS technologies like CloudFormation, DynamoDB, Lambda, EC2, SNS, and SQS",
-      "Developed Event-Driven Microservices using Apache Kafka",
-      "Reduced codebase size by 25% by transitioning to Serverless REST APIs using Python, Flask, and AWS Lambda",
-      "Designed attractive and user-friendly UI with Vue.js",
-      "Developed robust microservices for a massive web platform based on AWS using Python, Java, and Kotlin to provide functionality to hundreds of thousands of users internationally",
+      "Reduced hosting costs 10% by transitioning scheduling API to serverless using Python, Flask, and AWS Lambda + SQS",
+      "Developed attractive scheduling UI in a Vue.js frontend, interacting with a Kafka-based event-forwarding system",
+      "Rewrote schedule state management REST API in Python + Flask + RESTX, reducing codebase size by 30%",
+      "Standardized REST API Unit/Integration tests in Python by designing fixtures, decreasing future development time",
     ],
   },
   {
     title: "Software Engineering Intern",
-    company: "Creospark/Cloudspark Labs",
+    company: "Cloudspark Labs",
     date: "Jan - Apr 2022",
     bullets: [
-      "Designed and developed scalable RESTful and Event-Driven microservices for web apps leveraging Microsoft Azure",
-      "Developed core microservices in TypeScript using Nest.JS, CosmosDB, Microsoft Graph, and Dependency Injection",
-      "Led ground-up design and development of claims-based Authorization, Licensing, and Notification systems for MVP",
+      "Led ground-up design/development of RESTful and Event-Driven microservices for web apps leveraging Microsoft Azure",
+      "Leveraged Azure Service Bus to develop decoupled microservices, ensuring scalability with Azure autoscale",
+      "Led the development of Event-Driven Licensing, Notification, and Auth microservices for a start-up MVP launch, utilizing TypeScript, Nest.JS, CosmosDB, and Dependency Injection",
     ],
   },
   {
@@ -55,8 +62,7 @@ const workExperience: ExpBoxProps[] = [
     company: "Pillar To Post",
     date: "May - Aug 2021",
     bullets: [
-      "Designed and Developed an Automated Web Software Testing Framework in Selenium using Python and JavaScript",
-      "Saved hundreds of company hours by automating regression testing suite and eliminating manual testing",
+      "Designed and Developed an Automated Web Software Testing Framework in Selenium using Python and JavaScript, eliminating manual testing and saving 100+ company hours",
     ],
   },
 ];
