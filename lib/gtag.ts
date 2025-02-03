c5b759fe659166847615d5f3a515402d455e6223
Kanwarpal Brar <brar.kanwarpal.school@gmail.com>
2025-02-03 17:17:07 -0500

    proper analytics tag

---
+++ lib/gtag.ts
@@ -0,0 +1,20 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || 'G-6Y5ES8TQ3X'
+
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
+}
+
+export const event = ({ action, category, label, value }: {
+  action: string
+  category: string
+  label: string
+  value?: number
+}) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,27 @@ import Layout from "@/components/Layout/layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Chivo_Mono } from "next/font/google";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 const chivoMono = Chivo_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout>
       <main className={chivoMono.className}>
